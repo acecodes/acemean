4913ac43fa365a3e0338ac6af22c761310ea41cc
Ace <ace.github@gmail.com>
2015-04-02 14:19:15 -0700

    Added new parameters for schema fields

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -6,9 +6,37 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
-    password: String
+    email: {
+        type: String,
+        unique: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true
+    },
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+                return url;
+            }
+        }
+    }
+});
+
+UserSchema.set('toJSON', {
+    getters: true
 });
 
 mongoose.model('User', UserSchema);
